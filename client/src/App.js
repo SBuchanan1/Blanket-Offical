3de736cdb203e981567364c5f0cac73d7ce22388
SBuchanan1 <syd.buch@gmail.com>
2020-11-15 03:31:26 -0500

    affrf userform, success, confirm, and personal details

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,26 @@
 import React, { useState } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Title from './components/Title';
 import UploadForm from './components/UploadForm';
 import ImageGrid from './components/ImageGrid';
 import Modal from './components/Modal';
+// import Nav from "./components/Nav";
 
 
 function App() {
   const [selectedImg, setSelectedImg] = useState(null);
 
   return (
+    // <Router>
     <div className="App">
       <Title />
       <UploadForm />
       <ImageGrid setSelectedImg={setSelectedImg} />
-      { selectedImg && (
+      {selectedImg && (
         <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
       )}
     </div>
+    // <Router />
   );
 }
 
---
+++ client/src/components/UserForm.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class UserForm extends Component {
+    state = {
+        step 1,
+        firstName: '',
+        lastName: '',
+        email: '',
+        occupation: '',
+        city: '',
+        bio: '',
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default UderForm
