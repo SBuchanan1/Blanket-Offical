dbe11c284f9a1b6a9914ca6c1209a02868111004
SBuchanan1 <syd.buch@gmail.com>
2020-11-15 12:35:40 -0500

    changed console log response

--- client/src/components/Form/UserForm.js
+++ client/src/components/Form/UserForm.js
@@ -70,7 +70,7 @@ export class UserForm extends Component {
             case 4:
                 return <Success />;
             default:
-                (console.log('This is a multi-step form built with React.'))
+                (console.log('You, successfully created a build form for login.'))
         }
     }
 }
