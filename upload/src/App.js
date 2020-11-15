529a21c3bbd07ab85dbe57216a667ee16c1f3519
SBuchanan1 <syd.buch@gmail.com>
2020-11-14 21:27:25 -0500

    modified files

--- upload/src/App.js
+++ upload/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Title from './components/Title';
+import UploadForm from './components/UploadForm';
+import ImageGrid from './components/ImageGrid';
+import Modal from './components/Modal';
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+      <UploadForm />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      { selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   );
 }
 
 export default App;
+
---
+++ upload/src/firebase/config.js
@@ -0,0 +1,23 @@
+import * as firebase from 'firebase';
+import 'firebase/firebase-storage';
+import 'firebase/firebase-firestore';
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDKWI1f4J_81Au_UX4Cp9MPrrYOsOAir_s",
+    authDomain: "blanket-001.firebaseapp.com",
+    databaseURL: "https://blanket-001.firebaseio.com",
+    projectId: "blanket-001",
+    storageBucket: "blanket-001.appspot.com",
+    messagingSenderId: "1095318653360",
+    appId: "1:1095318653360:web:d45a4d5fb7a2b0ef18ee75",
+    measurementId: "G-FLBY5HH0CL"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
