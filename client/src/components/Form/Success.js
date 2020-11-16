4019454190fafa337c5cacfe8d8c15bfd4ca6c43
SBuchanan1 <syd.buch@gmail.com>
2020-11-15 20:34:19 -0500

    uncommented the success js

--- client/src/components/Form/Success.js
+++ client/src/components/Form/Success.js
@@ -1,45 +1,44 @@
-// import React, { Component } from 'react';
-// import Dialog from '@material-ui/core/Dialog';
-// import AppBar from '@material-ui/core/AppBar';
-// import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
-// import Button from '@material-ui/core/Button';
-// import deleteDialog from '../../hooks/DeleteDialog';
+import React, { Component } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import deleteDialog from '../../hooks/DeleteDialog';
 
 
 
 
-// export class Success extends Component {
-//     continue = e => {
-//         e.preventDefault();
-//         // PROCESS FORM //
-//         this.props.nextStep();
-//     };
+export class Success extends Component {
+    continue = e => {
+        e.preventDefault();
+        // PROCESS FORM //
+        this.props.nextStep();
+    };
 
-//     back = e => {
-//         e.preventDefault();
-//         this.props.prevStep();
-//     };
+    back = e => {
+        e.preventDefault();
+        this.props.prevStep();
+    };
 
-//     render() {
-//         return (
-//             <MuiThemeProvider>
-//                 <>
-//                     <Dialog
-//                         open
-//                         fullWidth
-//                         maxWidth='sm'
+    render() {
+        return (
+            <MuiThemeProvider>
+                <>
+                    <Dialog
+                        open
+                        fullWidth
+                        maxWidth='sm'
+                    >
+                        <AppBar title="Success" />
+                        <h1>Thank You For Your Submission</h1>
+                        <p>You will get an email with further instructions.</p>
+                        <Button onClick={() => deleteDialog(Dialog)} >CLICK</Button>
+                    </Dialog >
+                </>
+            </MuiThemeProvider >
+        );
+    }
+}
 
-//                     >
-//                         <AppBar title="Success" />
-//                         <h1>Thank You For Your Submission</h1>
-//                         <p>You will get an email with further instructions.</p>
-//                             <Button onClick={()=> deleteDialog(Dialog)} >CLICK</Button>   
-//                     </Dialog>
-//                 </>
-//             </MuiThemeProvider>
-//         );
-//     }
-// }
-
-// export default Success;
+export default Success;
 
